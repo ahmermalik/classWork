ab9d1717fab5903ebe2660e918dfc240133f58ae
Ahmer Malik <ahmer9800@gmail.com>
2017-09-23 10:14:33 -0500

    updates

--- Week1/exerciseselfstudy.py
+++ Week1/exerciseselfstudy.py
@@ -1,6 +1,38 @@
-import random
-import math
+#
+# fruit = "apple"
+# for idex in range(5):
+#         currentChar = fruit[idex]
+#         print(currentChar)
 
-s="pythonrocks"
+# fruit = "apple"
+# for idx in range(len(fruit) - 1, 1, -1):
+#         print(fruit[idx])
 
-print(len(s))
+
+
+# fruit = "dragons"
+#
+# position = 0
+# while position < len(fruit):
+#     print(fruit[position])
+#     position = position + 1
+#
+#
+# s = "python rocks"
+# idx = 1
+# while idx < len(s):
+#     print(s[idx])
+#     idx = idx + 2
+
+
+def removeVowels(s):
+    vowels = "aeiouAEIOU"
+    sWithoutVowels = ""
+    for eachChar in s:
+        if eachChar not in vowels:
+            sWithoutVowels = sWithoutVowels + eachChar
+    return sWithoutVowels
+
+
+print(removeVowels("compsci motha fuckaahhh this shit is great"))
+print(removeVowels("aAbEefIijOopUus"))
