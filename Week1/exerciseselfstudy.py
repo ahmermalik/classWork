dac34468fec819ceebb84b51b4d174f2a4cdbbcc
Ahmer Malik <ahmer9800@gmail.com>
2017-09-24 10:34:00 -0500

    added updates

--- Week1/exerciseselfstudy.py
+++ Week1/exerciseselfstudy.py
@@ -25,14 +25,68 @@
 #     idx = idx + 2
 
 
-def removeVowels(s):
-    vowels = "aeiouAEIOU"
-    sWithoutVowels = ""
-    for eachChar in s:
-        if eachChar not in vowels:
-            sWithoutVowels = sWithoutVowels + eachChar
-    return sWithoutVowels
+# def removeVowels(s):
+#     vowels = "aeiouAEIOU"
+#     sWithoutVowels = ""
+#     for eachChar in s:
+#         if eachChar not in vowels:
+#             sWithoutVowels = sWithoutVowels + eachChar
+#     return sWithoutVowels
+#
+#
+# print(removeVowels("This sentences has a few vowels, but not all"))
+# print(removeVowels("aAbEefIijOopUus"))
+#
 
 
-print(removeVowels("compsci motha fuckaahhh this shit is great"))
-print(removeVowels("aAbEefIijOopUus"))
+
+# s = "ball"
+# r = ""
+# for item in s:
+#     r = item.upper() +r
+# print(r)
+#
+
+
+def applyRules(lhch):
+    rhstr = ""
+    if lhch == 'A':
+        rhstr = 'B'   # Rule 1
+    elif lhch == 'B':
+        rhstr = 'AB'  # Rule 2
+    else:
+        rhstr = lhch    # no rules apply so keep the character
+
+    return rhstr
+
+
+def processString(oldStr):
+    newstr = ""
+    for ch in oldStr:
+        newstr = newstr + applyRules(ch)
+
+    return newstr
+
+
+def createLSystem(numIters,axiom):
+    startString = axiom
+    endString = ""
+    for i in range(numIters):
+        endString = processString(startString)
+        startString = endString
+
+    return endString
+
+print(createLSystem(4, "A"))
+
+
+def drawLsystem(aTurtle,instructions,angle,distance):
+    for cmd in instructions:
+        if cmd == 'F':
+            aTurtle.forward(distance)
+        elif cmd == 'B':
+            aTurtle.backward(distance)
+        elif cmd == '+':
+            aTurtle.right(angle)
+        elif cmd == '-':
+            aTurtle.left(angle)
