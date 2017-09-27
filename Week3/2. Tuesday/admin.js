d972407a611c20a27ea93757c7ac04e0672bddf3
Ahmer Malik <ahmer9800@gmail.com>
2017-09-27 15:02:07 -0500

    ahmer

--- Week3/2. Tuesday/admin.js
+++ Week3/2. Tuesday/admin.js
@@ -1,3 +1,16 @@
 /**
  * Created by ahmermalik on 9/26/17.
  */
+function animationHover(element, animation){
+    element = $(element);
+    element.hover(
+        function() {
+            element.addClass('animated ' + animation);
+        },
+        function(){
+            //wait for animation to finish before removing classes
+            window.setTimeout( function(){
+                element.removeClass('animated ' + animation);
+            }, 2000);
+        });
+}
