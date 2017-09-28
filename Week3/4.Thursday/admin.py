b55d7df4a1cdd2a3a816378f5d781753658ea60d
Ahmer Malik <ahmer9800@gmail.com>
2017-09-28 12:40:09 -0500

    tornado hobby

--- Week3/4.Thursday/admin.py
+++ Week3/4.Thursday/admin.py
@@ -1,2 +1,19 @@
 import tornado.ioloop
 import tornado.web
+import tornado.log
+
+class MainHandler(tornado.web.RequestHandler):
+  def get(self):
+    self.write("Hello, world")
+def make_app():
+  return tornado.web.Application([
+    (r"/", MainHandler),
+  ], autoreload=True)
+
+if __name__ == "__main__":
+  tornado.log.enable_pretty_logging()
+
+  app = make_app()
+  app.listen(8888)
+  tornado.ioloop.IOLoop.current().start()
+  
