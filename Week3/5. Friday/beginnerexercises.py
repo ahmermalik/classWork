c6dbe01f96a61e6dcc0f8e5f0c06f4c2f78f1dd6
Ahmer Malik <ahmer9800@gmail.com>
2017-09-30 18:40:13 -0500

    updates

--- Week3/5. Friday/beginnerexercises.py
+++ Week3/5. Friday/beginnerexercises.py
@@ -1,5 +1,34 @@
+#
+# name = input('What is your name?')
+# age = input(str('what is your age? '))
+#
+# print('Your name is ' +name +' and your age is ' +age)
 
-name = input('What is your name?')
-age = input(str('what is your age? '))
 
-print('Your name is ' +name +' and your age is ' +age)
+# num = int(input("Give me a number: "))
+#
+# mod = num % 2
+#
+# if mod > 0:
+#     print("You picked an odd number.")
+#
+# else:
+#     print("You picked an even number.")
+
+# x =[]
+# a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+#
+#
+#
+# for numbers in a:
+#     if numbers < 40:
+#         x.append(numbers)
+# print(x)
+#
+
+userint = str(input('What number do you want to divide?'))
+x = range(1, 30)
+newlist=[]
+for number in x:
+    if number % userint == 0:
+        newlist.append[x]
