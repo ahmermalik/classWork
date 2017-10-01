c827e010035a9ab2f2bd0ad0dbd1cca0b935ad64
Ahmer Malik <ahmer9800@gmail.com>
2017-10-01 17:07:28 -0500

    revamp directories

---
+++ Javascript/Exercises/jsexercises.js
@@ -0,0 +1,40 @@
+/**
+ * Created by ahmermalik on 9/30/17.
+ */
+
+
+function isPrime(n){
+    var divisor = 2;
+
+    while (n > divisor){
+        if(n % divisor == 0){
+            return false;
+        }
+        else
+            divisor++;
+    }
+    return true;
+}
+
+
+console.log(isPrime(137));
+
+//
+//
+//
+// function primeFactors(n){
+//     var factors = [],
+//         divisor = 2;                //establish the empty array and your divisor
+//
+//     while(n>2){                     //initialize a while loop that takes 'n' aka whatever number you want to know the prime factor of
+//         if(n % divisor == 0){       // if 'n' divided into 2 is not a whole number, it goes to else.
+//             factors.push(divisor);
+//             n= n/ divisor;             //if it has a remainder of 0, push it into the empty array.
+//         }
+//         else{
+//             divisor++;              //it goes up in number..after being initizlied at 2.
+//         }
+//     }
+//     return factors;
+// }
+// console.log(primeFactors(69))
---
+++ Javascript/cryptomania/server.js
@@ -0,0 +1,11 @@
+/**
+ * Created by ahmermalik on 9/30/17.
+ */
+
+
+const ccxt = require ('ccxt')
+
+
+console.log (await exchange.loadMarkets ())
+
+let btcusd1 = bittrex.markets['BTC/USD']
