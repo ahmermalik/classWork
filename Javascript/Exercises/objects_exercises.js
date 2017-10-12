2b2bbe034b22f289eda20ba9b97f406127ce98da
Ahmer Malik <ahmer9800@gmail.com>
2017-10-12 12:27:19 -0500

    added objects exercises

--- Javascript/Exercises/objects_exercises.js
+++ Javascript/Exercises/objects_exercises.js
@@ -1,5 +1,7 @@
 
+console.log("===================================================")
 
+console.log("                   Phonebook")
 var phonebookDict = {
     Alice: '703-493-1834',
     Bob: '857-384-1234',
@@ -33,3 +35,40 @@ console.log("Bob's new Phone nubmer is " + phonebookDict.Bob+ ".\n")
 console.log("The most recent phonebook is: \n")
 console.log(phonebookDict)
 
+console.log("===================================================")
+
+console.log("                   Letter Histogram")
+
+console.log("How many times the letter 'a' is in the sentence: 'A man is as good as his word'")
+
+str = "A man is as good as his word";
+console.log((str.split('a').length-1));
+
+
+// The string
+var str = "I want to count the number of occurances of each char in this string";
+
+// A map (in JavaScript, an object) for the character=>count mappings
+var counts = {};
+
+// Misc vars
+var ch, index, len, count;
+
+// Loop through the string...
+for (index = 0, len = str.length; index < len; ++index) {
+    // Get this character
+    ch = str.charAt(index); // Not all engines support [] on strings
+
+    // Get the count for it, if we have one; we'll get `undefined` if we
+    // don't know this character yet
+    count = counts[ch];
+
+    // If we have one, store that count plus one; if not, store one
+    // We can rely on `count` being falsey if we haven't seen it before,
+    // because we never store falsey numbers in the `counts` object.
+    counts[ch] = count ? count + 1 : 1;
+}
+
+for (ch in counts) {
+    console.log(ch + " count: " + counts[ch]);
+}
