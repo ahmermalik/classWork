677d1f4d571cd541c8468431280561f8f7250e6c
Ahmer Malik <ahmer9800@gmail.com>
2017-10-12 12:07:47 -0500

    updates

--- Javascript/cryptomania/server.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by ahmermalik on 9/30/17.
- */
-
-
-
-
-const ccxt = require ('ccxt')
-let exchange = new ccxt.kraken () // default id
--- Week3/4.Thursday/exercise.py
+++
@@ -1,6 +0,0 @@
-string = 'bananas'
-counts_dict = {}
-
-for char in string:
-    counts_dict[char] = counts_dict[char] + 1
-    print(counts_dict)
