62f6285df78973cba21748cb72976681442485ed
Ahmer Malik <ahmer9800@gmail.com>
2017-10-26 23:52:02 -0500

    add exercises

--- Javascript/Exercises/Oct26exercise.js
+++ Javascript/Exercises/Oct26exercise.js
@@ -2,77 +2,195 @@
  * Created by ahmermalik on 10/26/17.
  */
 
-// filename ='jsexercises.js'
+
+
 //
-// var fs = require('fs');
-// fs.readFile(filename, function (error, buffer) {
-//     if (error) {
-//         console.error(error.message);
-//         return;
-//     }
-//     console.log('File Data: ', buffer.toString());
+//                                 // Callback Exercises
+//                                 //Function Rewrites
+//
+//
+//
+// //Function 1
+// function add(x, y) {
+//     var result = x + y;
+//     return x+y;
+// }
+//
+//
+// // console.log(add(2,3))
+//
+// let calc_add = function(num1, num2, callback) {
+//     return callback(num1, num2, add)
+// }
+//
+// console.log(calc_add(2,3,add));
+//
+//
+// //Function 2
+// function subtract(x, y) {
+//     return x - y;
+// }
+//
+// let calc_subtract = function(num1, num2, callback) {
+//     return callback(num1,num2, subtract)
+// }
+//
+// console.log(calc_subtract(25,3, subtract))
+//
+//
+// //Function 3
+// function greeting(person){
+//     return 'Hola, ' + person + '!';
+//  }
+//
+//  let greet_func = function(person1, callback){
+//     return callback(person1, greeting)
+//
+//  }
+//
+//  console.log(greet_func('Ahmer', greeting))
+//
+//
+//
+// function product(numbers) {
+//     return numbers.reduce(function(a, b) {
+//         return a * b;
+//     }, 1);
+// }
+//
+//
+// let prod_func = function(num1, num2, callback){
+//     return callback(num1, num2, product)
+// }
+//
+
+// var readline = require('readline');
+// var rl = readline.createInterface(process.stdin, process.stdout);
+// rl.setPrompt('guess> ');
+// rl.prompt();
+// rl.on('line', function(line) {
+//     if (line === "right") rl.close();
+//     rl.prompt();
+// }).on('close',function(){
+//     process.exit(0);
 // });
 
+const readline = require('readline');
+const fs = require('fs');
+const dns = require('dns');
 
+/**
+ * Get input from console
+ * @returns {Promise}
+ */
+function getInput(questionString){
+    return new Promise(function(resolve, reject){ // promises help prevent callback hell
+        const rl = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout
+        });
 
-                                // Callback Exercises
-                                //Function Rewrites
+        rl.question(questionString, (answer) => {
+            resolve(answer);
 
-
-
-//Function 1
-function add(x, y) {
-    var result = x + y;
-    return x+y;
+            rl.close();
+        });
+    });
 }
 
-
-// console.log(add(2,3))
-
-let calc_add = function(num1, num2, callback) {
-    return callback(num1, num2, add)
+/**
+ * Read file
+ * @param fileName
+ */
+function readFile(fileName){
+    return new Promise(function(resolve, reject){
+        fs.readFile(fileName, 'utf8', function (error, buffer) {
+            if (error) {
+                reject(error.message);
+            } else {
+                resolve(buffer.toString());
+            }
+        });
+    });
 }
 
-console.log(calc_add(2,3,add));
-
-
-//Function 2
-function subtract(x, y) {
-    return x - y;
+/**
+ * Write a file
+ * @param fileName (string) file name
+ * @param text (string) the text that the file will have
+ */
+function writeFile(fileName, text){
+    return new Promise(function(resolve, reject){
+        fs.writeFile(fileName, text, function(err){
+            if(err){
+                reject(err);
+            } else {
+                resolve();
+            }
+        });
+    });
 }
 
-let calc_subtract = function(num1, num2, callback) {
-    return callback(num1,num2, subtract)
+/**
+ * Run the program
+ */
+function runFileReader(){
+    getInput('Enter a filename: ')
+        .then(function(userInput){ // read file
+            var fileName = userInput;
+
+            return readFile(fileName);
+        })
+        .then(function(fileContents){
+            console.log(fileContents);
+        })
+        .catch(function(err){
+            console.log(' got an error '+err);
+        });
 }
 
-console.log(calc_subtract(25,3, subtract))
-
-
-//Function 3
-function greeting(person){
-    return 'Hola, ' + person + '!';
- }
-
- let greet_func = function(person1, callback){
-    return callback(person1, greeting)
-
- }
-
- console.log(greet_func('Ahmer', greeting))
-
-
-
-function product(numbers) {
-    return numbers.reduce(function(a, b) {
-        return a * b;
-    }, 1);
+function runDNSLookup(){
+    getInput('Domain name: ')
+        .then(function(domainName){ // read file
+            return dns.lookup(domainName, null, function(err, ip){
+                if(err){
+                    console.log('Uh oh: '+err);
+                } else {
+                    console.log(ip);
+                }
+            });
+        })
+        .catch(function(err){
+            console.log(' got an error '+err);
+        });
 }
 
+function runReadWrite(){
+    var inputFileName;
+    var outputFileName;
 
-let prod_func = function(num1, num2, callback){
-    return callback(num1, num2, product)
+    getInput("Enter a file name to read: ")
+        .then(function(fileName){
+            inputFileName = fileName;
+
+            return getInput("Enter output file name: ");
+        })
+        .then(function(fileName){
+            outputFileName = fileName;
+
+            return readFile(inputFileName);
+        })
+        .then(function(fileContents){
+            return writeFile(outputFileName, fileContents);
+        })
+        .then(function(){
+            console.log("You got a new file!");
+        })
+        .catch(function(err){
+            console.log('error: '+err);
+        });
 }
 
-// console.log(prod_func(3,2, product))
-//
-r
+runReadWrite();
+
+
