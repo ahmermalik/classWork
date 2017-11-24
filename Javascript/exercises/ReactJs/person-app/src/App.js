f2d26c419787d7c3b36152fe2c0814541a81d02f
Ahmer Malik <ahmer9800@gmail.com>
2017-11-24 00:29:16 -0600

    add switch button

--- Javascript/exercises/ReactJs/person-app/src/App.js
+++ Javascript/exercises/ReactJs/person-app/src/App.js
@@ -4,11 +4,28 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons :[
+        {name: 'JP', age: 26 },
+        {name: 'Azael', age: 26},
+        {name: 'Carolina', age: 25}
+    ]
+  }
+
+  switchNameHandler = () => {
+    console.log('You clicked a button!');
+}
+
+
   render() {
     return (
       <div className = "App">
           <h1> Hi, this is a react app </h1>
           <p> This is really working! </p>
+          <button onClick={this.switchNameHandler}>Switch Name </button>
+          <Person name = {this.state.persons[0].name} age = {this.state.persons[0].age}></Person>
+          <Person name = {this.state.persons[1].name} age = {this.state.persons[1].age}></Person>
+          <Person name = {this.state.persons[2].name} age = {this.state.persons[2].age}></Person>
           <Person name = "Ahmer" age = "28"> My Hobbies: Studying, Learning, Coding, and Trading </Person>
           <Person name = "Ashar" age = "25"> My Hobbies: Startup, Reading, Learning, and Trading </Person>
           <Person name = "Samira" age = "58"> My Hobbies: Cooking, Laughing, Gardening, and Reading </Person>
@@ -16,5 +33,7 @@ class App extends Component {
     );
   }
 }
+//state is a special property that allows you to pass in data from an array into a component.
+//hobbies are known as children of the component
 
 export default App;
